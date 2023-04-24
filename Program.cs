5c8330d8fb98a31aaf38695adf24ae24b02d5a9e
Pang <narumonokanoto@gmail.com>
2023-04-24 10:53:45 +0700

    i1

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        string[] type = ; 
+        string[] item = ; 
+        
+     do {
+        PrintType(type);
+        PrintShowAll(type);
+            
+        }
+    while
+
+    break;
+    }
+
+
+    static void PrintType(string[] item) {
+            Console.WriteLine("Item : ");
+            string InputItem = Console.ReadLine();
+
+            if (InputItem == "LongSword" || InputItem == "ShortSword") {
+                Console.WriteLine("Type : {0}",type[0]);
+            }else if (InputItem == "Lollipop") {
+                Console.WriteLine("Type : {1}",type[1]);
+            }else if (InputItem == "GrilledFish") {
+                Console.WriteLine("Type : {2}",type[2]);
+            }else if (InputItem == "Potionh") {
+                Console.WriteLine("Type : {3}",type[3]);
+            }else {
+                Console.WriteLine("End");
+            }
+    }
+
+    static void PrintShowAll(string[] type) {
+            Console.WriteLine("{0}",type[0]);
+            Console.WriteLine(" ");
+            Console.WriteLine("{1}",type[1]);
+            Console.WriteLine(" ");
+            Console.WriteLine("{2}",type[2]);
+            Console.WriteLine(" ");
+            Console.WriteLine("{3}",type[3]);
+            Console.WriteLine(" ");
+            Console.WriteLine("{4}",type[4]);
+            Console.WriteLine(" ");
+            Console.WriteLine("END");
+    }
+
+
+    static void PrintType(string type) {
+           int[] numtype = num int[10]; 
+        for (int i = 0; i < 10; ++i) {
+            num[i] = int.Parse(Console.ReadLine());
+
+        }
+        while(true) {
+                break;
+            }
+    }
+
+    
+    static void PrintItem(string item) {
+           string[] item = int[10]; 
+        for (int i = 0; i < 10; ++i) {
+        }
+        while(true) {
+                break;
+            }
+
+    }
+}
+
+//byebyeนะพี่ ผมเมากับ Array ไม่ไหวแล้ววววว ลาก่อนนนะพี่//
